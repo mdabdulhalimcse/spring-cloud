a6106de22d2855615396528d384a70630b241fb4
unknown <mdabdulhalimcse@gmail.com>
2022-01-11 21:59:26 +0600

    microservice basic from hard code

---
+++ employee-producer/src/main/java/com/abdulhalim/controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.abdulhalim.controller;
+
+import com.abdulhalim.model.Employee;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public Employee firstPage() {
+
+        Employee emp = new Employee();
+        emp.setName("emp1");
+        emp.setDesignation("manager");
+        emp.setEmpId("1");
+        emp.setSalary(3000);
+
+        return emp;
+    }
+}
---
+++ emplyee-consumer/src/test/java/com/abdulhalim/EmplyeeConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdulhalim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmplyeeConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
