0cafff138b4090db2dfe302b92c68ff5f57ed11c
unknown <mdabdulhalimcse@gmail.com>
2022-01-11 22:36:32 +0600

    Eureka Server Register Service

--- emplyee-consumer/src/main/java/com/abdulhalim/EmplyeeConsumerApplication.java
+++ emplyee-consumer/src/main/java/com/abdulhalim/EmplyeeConsumerApplication.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 import com.abdulhalim.controller.ConsumerControllerClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestClientException;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EmplyeeConsumerApplication {
 
     public static void main(String[] args) throws RestClientException, IOException {
